126eba68dd4cab942a67b911b3caafd404f818ac
Pietro Martinelli <amicofragile@gmail.com>
2016-04-24 18:17:29 +0200

    REFACTORING Added support for functions with arbitrary argument number

--- src/decorator.js
+++ src/decorator.js
@@ -2,14 +2,23 @@ function factory(options) {
    const before = options.before
    const after = options.after
    return function decorator(fn) {
-      return function proxy(x, y, callback) {
-         before(x, y, function() {
-            fn(x, y, function(result) {
-               after(x, y, result, function() {
+      return function proxy() {
+         const proxyArgs = Array.prototype.slice.call(arguments, 0, arguments.length - 1)
+         const beforeArgs = Array.prototype.slice.call(proxyArgs)
+         const callback = arguments[arguments.length - 1]
+         beforeArgs.push(function() {
+            const fnArgs = Array.prototype.slice.call(proxyArgs)
+            fnArgs.push(function(result) {
+               const afterArgs = Array.prototype.slice.call(proxyArgs)
+               afterArgs.push(result)
+               afterArgs.push(function() {
                   callback(result)
                })
+               after.apply(null, afterArgs)
             })
+            fn.apply(null, fnArgs)
          })
+         before.apply(null, beforeArgs)
       }
    }
 }
